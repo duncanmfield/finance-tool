46c5bb38f3ead17f8245519f0ade93193d35b69c
Duncan Field <duncanmfield@gmail.com>
2020-06-10 19:21:32 +0100

    Modified accounts to be linked to a specific user, and several minor tweaks

--- app/urls.py
+++ app/urls.py
@@ -3,13 +3,14 @@
 MIT License
 Copyright (c) 2019 - present AppSeed.us
 """
-
+from django.conf.urls import url
 from django.urls import path, re_path
 from app import views
 
 urlpatterns = [
     path('accounts.html', views.AccountListView.as_view(), name='accounts'),
     path('account_create.html', views.AccountCreateView.as_view(), name='account_create'),
+    url(r'^account_delete/(?P<pk>\d+)/$', views.AccountDeleteView.as_view(), name='delete_view'),
 
     # Matches any html file - to be used for gentella
     # Avoid using your .html in your resources.
--- app/views.py
+++ app/views.py
@@ -3,15 +3,17 @@
 MIT License
 Copyright (c) 2019 - present AppSeed.us
 """
-
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from django import template
 from django.utils import timezone
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 
 from app.forms import AccountCreateForm
 from app.models import Account
@@ -26,34 +28,42 @@ def pages(request):
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
     try:
-        
         load_template = request.path.split('/')[-1]
         html_template = loader.get_template( load_template )
         return HttpResponse(html_template.render(context, request))
-        
     except template.TemplateDoesNotExist:
-
         html_template = loader.get_template( 'page-404.html' )
         return HttpResponse(html_template.render(context, request))
-
     except:
-    
         html_template = loader.get_template( 'page-500.html' )
         return HttpResponse(html_template.render(context, request))
 
 
-class AccountCreateView(CreateView):
+class AccountCreateView(LoginRequiredMixin, CreateView):
     form_class = AccountCreateForm
     template_name = 'account_create.html'
     success_url = 'accounts.html'
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
-class AccountListView(ListView):
+
+class AccountListView(LoginRequiredMixin, ListView):
     template_name = 'accounts.html'
     model = Account
-    paginate_by = 25
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
+
+class AccountDeleteView(LoginRequiredMixin, DeleteView):
+    model = Account
+    success_url = '/accounts.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
