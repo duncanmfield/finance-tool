5fb7ecaec1233fe19c1f5893c4474e1fe42ab01f
Duncan Field <duncanmfield@gmail.com>
2020-07-21 23:05:24 +0100

    Removed pragma no cover tag, coverage 83% -> 82%

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:  # pragma: no cover
+    except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
